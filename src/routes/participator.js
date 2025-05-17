d3fe067d8180d70f430cb6e9acf85028f7427fad
Lucas Carvalho <lucascarvalhosilva2901@gmail.com>
2025-05-17 18:28:24 -0300

    adjustment search participator by Id

--- src/routes/participator.js
+++ src/routes/participator.js
@@ -7,7 +7,7 @@ const routes = express.Router();
 routes.post("/participator",  Participator.createParticipator);
 //routes.delete("/participator/:id", Auth.authenticate, Auth.permission(2), Ceremony.deleteBoardGame);
 //routes.get("/participator", Auth.authenticate, Auth.permission(1), Ceremony.listAllCeremonies);
-//routes.get("/participator/:id", Auth.authenticate, Auth.permission(1), Ceremony.searchCeremonyByID);
+routes.get("/participator/:id", Participator.searchparticipatorByID);
 //routes.get("/participator/search", Auth.authenticate, Auth.permission(1), Ceremony.searchBoardGameByName);
 
 export default routes
