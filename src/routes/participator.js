852e1a753d4d1dd0aac45cd41796cf32bfc8c80e
Lucas Carvalho <lucascarvalhosilva2901@gmail.com>
2025-05-17 19:26:30 -0300

    search by identifier

--- src/routes/participator.js
+++ src/routes/participator.js
@@ -5,9 +5,7 @@ import Auth from "../middlewares/permission.js";
 const routes = express.Router();
 
 routes.post("/participator",  Participator.createParticipator);
-//routes.delete("/participator/:id", Auth.authenticate, Auth.permission(2), Ceremony.deleteBoardGame);
-//routes.get("/participator", Auth.authenticate, Auth.permission(1), Ceremony.listAllCeremonies);
+routes.get("/participator/search",  Participator.searchparticipatorByDocument);
 routes.get("/participator/:id", Participator.searchparticipatorByID);
-//routes.get("/participator/search", Auth.authenticate, Auth.permission(1), Ceremony.searchBoardGameByName);
 
 export default routes
