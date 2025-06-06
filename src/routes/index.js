6334cf9fa23840b57f391f3bd73ae0e8504b0a9a
Lucas Carvalho <lucascarvalhosilva2901@gmail.com>
2025-06-06 20:51:17 -0300

    feat: list all roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,11 +11,12 @@ import oneShot from "./oneShots.js";
 import scapeRoomHistory from "./scapeRoomHistory.js";
 import scapeRoomSession from "./scapeRoomSession.js";
 import campaing from "./campaing.js";
+import role from "./role.js";	
 
 const routes = (app) => {
   app.route('/').get((req, res) => res.status(200).send("Ludico Server! Welcome"));
 
-  app.use(express.json(), user, login, ceremony, boardgame, lent, participator, rpgSystem, character, oneShot, scapeRoomHistory, scapeRoomSession, campaing);
+  app.use(express.json(), user, login, ceremony, boardgame, lent, participator, rpgSystem, character, oneShot, scapeRoomHistory, scapeRoomSession, campaing, role);
 }
 
 export default routes;
